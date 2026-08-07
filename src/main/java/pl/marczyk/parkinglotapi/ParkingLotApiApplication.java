7a4ca87a818fea89d2a557c07032ac531ff5ea25
Marcin Marczyk <m.l.marczyk@gmail.com>
2026-08-07 18:24:55 +0200

    initial commit

---
+++ src/main/java/pl/marczyk/parkinglotapi/ParkingLotApiApplication.java
@@ -0,0 +1,13 @@
+package pl.marczyk.parkinglotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/marczyk/parkinglotapi/ParkingLotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marczyk.parkinglotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
